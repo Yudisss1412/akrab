05454a36ba6e68b3c78f85cdeae8aade9ec8019a
Yudisss1412 <dwianggara1412@gmail.com>
2025-12-11 05:39:56 +0700

    responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Inter:wght@400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
 </head>
 <body class="js-enabled">
     <div class="register-container">

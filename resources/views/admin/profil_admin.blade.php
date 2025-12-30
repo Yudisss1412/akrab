d455e9d702cb7593abe0291746a8af11df1fa9b2
Yudisss1412 <dwianggara1412@gmail.com>
2025-12-31 02:15:44 +0700

    Memperbaiki tampilan card profil admin dan log personal agar menyesuaikan lebar layar penuh, serta memindahkan styling ke file khusus untuk profil admin agar lebih terorganisir

--- resources/views/admin/profil_admin.blade.php
+++ resources/views/admin/profil_admin.blade.php
@@ -6,6 +6,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/penjual/profil_penjual.css') }}">
   <link rel="stylesheet" href="{{ asset('css/admin_penjual/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/admin/profil_admin.css') }}">
 @endpush
 
 @section('content')
